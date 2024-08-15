98fb1a1b65eba5c71f250c7eed6273d411943d5b
anchu-jossy <anchujossy@gmail.com>
2024-08-15 19:16:45 +0100

    implemented catalog dependency management

--- app/src/main/java/com/example/wizardsofhogwarts/ui/theme/Type.kt
+++ app/src/main/java/com/example/wizardsofhogwarts/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.wizardsofhogwarts.ui.theme
+package com.stevdza.san.themeswitcher.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
